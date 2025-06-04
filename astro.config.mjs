632e40b237443f5a76181281c6b85a658005d464
sam <sarellano@centro.edu.mx>
2025-06-04 06:32:51 -0700

    Update deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://github.com/sam25549/AstroWinStar",
+  base: "/AstroWinStar",
+  trailingSlash: "never",
+  outDir: "docs",
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  typescript: {
+    strict: false,
+    check: false
+  }
 });
