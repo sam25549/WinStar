c4d61e1147fe66e3965e7857980de4297a89b789
JuanFuent.es <juan.fuentes.cabrera.89@gmail.com>
2025-06-05 22:57:53 -0600

    Update Astro Configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://sam25549.github.io/WinStar",
+  base: "/WinStar",
+  trailingSlash: "never",
+  outDir: "docs",
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  typescript: {
+    strict: false,
+    check: false
+  }
 });
